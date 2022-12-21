ed70ac4b1ed5c41cd9ad678ea332f3b1b5cb0f5b
Andrey <andslob@inbox.ru>
2022-12-21 16:14:42 +0500

    lkjnb

---
+++ Examples/Example_005/Program.cs
@@ -0,0 +1,11 @@
+﻿// Int numberA = new Random().Next(1,10);
+// Int numberB = new Random().Next(1,10);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);

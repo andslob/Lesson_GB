eea538affb88ba499bcc80b4f248f6131f6e99e4
Andrey <andslob@inbox.ru>
2022-12-21 16:15:36 +0500

    qwe

---
+++ Examples/Example_003/Program.cs
@@ -0,0 +1,8 @@
+﻿// int numberA = 3;
+// int numberВ = 5;
+// Console.WriteLine(numberA + numberВ );
+
+int numberA = 31;
+int numberВ = 5;
+int result = numberA + numberВ;
+Console.WriteLine(result);
